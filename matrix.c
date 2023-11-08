ddc53e2cbcdc46abbafcc860ad9946a79faa30bb
Tysane Marcelina <tysanema@ox-b1.msk.21-school.ru>
2023-11-08 15:08:38 +0300

    Files

--- matrix.c
+++ matrix.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int matrix_n(int **table, int *rows,  int i, int j, int n, int m);
-int matrix_m(int **table, int *rows, int i, int j, int n, int m);
+int matrix_n(int **table, int *rows,  int i, int j, int n, int m, int tmp);
+int matrix_m(int **table, int *rows, int i, int j, int n, int m, int tmp);
 
 int main(void)
 {
     int **table;    // указатель для блока памяти для массива указателей
     int *rows;      // указатель для блока памяти для хранения информации по строкам
- 
+    int tmp=0; // вводилось ли значение
     int n, m = 0; // Последнее число
     int i, j = 0; // Текущее число
     // ввод количества строк
@@ -18,7 +18,8 @@ int main(void)
     table = calloc(n, sizeof(int*));
     rows = malloc(sizeof(int)*n);
 
-    matrix_n(table, rows, i, j, n, m);    
+    matrix_n(table, rows, i, j, n, m, tmp);
+    matrix_n(table, rows, i, j, n, m, tmp);
     // освобождение памяти
     free(table);
     free(rows);
@@ -26,51 +27,46 @@ int main(void)
     return 0;
 }
 
-int matrix_n(int **table, int *rows, int i, int j, int n, int m){
-    if(i<n&&table[i]==NULL){
+int matrix_n(int **table, int *rows, int i, int j, int n, int m, int tmp){
+    if(i<n&&tmp==0){
         printf("\nColumns count for row %d=", i);
         scanf("%d", &rows[i]);
         table[i] = calloc(rows[i], sizeof(int));
         int j=0;
-        matrix_m(table, rows, n, m, i, j);
+        matrix_m(table, rows, n, m, i, j, tmp);
         i++;
-        matrix_n(table, rows, n, m, i, j);
+        matrix_n(table, rows, n, m, i, j, tmp);
     }
-    else if(i<n&&table[i]!=NULL) {
+    else if(i<n&&tmp==1) {
         printf("\n");
-        matrix_m(table, rows, n, m, i, j);
+        matrix_m(table, rows, n, m, i, j, tmp);
+        i++;
+        matrix_n(table, rows, n, m, i, j, tmp);
     }
     else{
         i=0;
         free(table[i]);
     }
+    tmp=1;
     return 0;
 }
-int matrix_n_out(){
-    if (i<n){
-        printf("\n");
-        matrix_m_out();
-        matrix_n_out();
-        i++;
-    }
-}
 
-int matrix_m(int **table, int *rows, int i, int j, int n, int m) {
-    if(j<rows[i]&&!table[i][j]==NULL){
+int matrix_m(int **table, int *rows, int i, int j, int n, int m, int tmp) {
+    if(j<rows[i]&&tmp==0){
         printf("table[%d][%d]=", i, j);
         scanf("%d", &m);
         table[i][j] = m;
+        j++;
+        matrix_m(table, rows, i, j, n, m, tmp);
     }
-    else if(j<rows[i]&&table[i][j]!=NULL) {
+    else if(j<rows[i]&&tmp==1) {
         printf("%d \t", table[i][j]);
+        j++;
+        matrix_m(table, rows, n, m, i, j, tmp);
+
     }
     else{
         j=0;
     }
     return 0;
 }
-int matrix_m_out(){
-    if (i<m){
-        printf("%d \t", table[i][j]);
-    }
-};
