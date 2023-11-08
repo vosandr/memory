5aa4c43bf718a43e3e7e7a150dfc45f04537e4d6
Tysane Marcelina <tysanema@ga-p2.msk.21-school.ru>
2023-11-08 15:58:45 +0300

    Files

--- matrix.c
+++ matrix.c
@@ -8,19 +8,15 @@ int main(void)
 {
     int **table;    // указатель для блока памяти для массива указателей
     int *rows;      // указатель для блока памяти для хранения информации по строкам
-    int tmp=0; // вводилось ли значение
-    int n, m = 0; // Последнее число
-    int i, j = 0; // Текущее число
-    // ввод количества строк
+    int tmp = 0; // вводилось ли значение
+    int n, m = 1; // Последнее число
+    int i, j = 1; // Текущее число
     printf("Rows count=");
     scanf("%d", &n);
-    // выделяем память для двухмерного массива
     table = calloc(n, sizeof(int*));
     rows = malloc(sizeof(int)*n);
-
     matrix_n(table, rows, i, j, n, m, tmp);
     matrix_n(table, rows, i, j, n, m, tmp);
-    // освобождение памяти
     free(table);
     free(rows);
  
@@ -28,7 +24,7 @@ int main(void)
 }
 
 int matrix_n(int **table, int *rows, int i, int j, int n, int m, int tmp){
-    if(i<n&&tmp==0){
+    if(i<=n&&tmp==0){
         printf("\nColumns count for row %d=", i);
         scanf("%d", &rows[i]);
         table[i] = calloc(rows[i], sizeof(int));
@@ -52,7 +48,7 @@ int matrix_n(int **table, int *rows, int i, int j, int n, int m, int tmp){
 }
 
 int matrix_m(int **table, int *rows, int i, int j, int n, int m, int tmp) {
-    if(j<rows[i]&&tmp==0){
+    if(j<=rows[i]&&tmp==0){
         printf("table[%d][%d]=", i, j);
         scanf("%d", &m);
         table[i][j] = m;
