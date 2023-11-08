4f0002a64675f3075b54b88d9bddf44efd564d60
Tysane Marcelina <tysanema@ox-b1.msk.21-school.ru>
2023-11-08 14:08:58 +0300

    Files

--- matrix.c
+++ matrix.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int array_rows(int **table, int *rows, int rowscount, int i);
+int array_rows(int **table, int *rows, int n, int m, int i, int j);
 
 int main(void)
 {
     int **table;    // указатель для блока памяти для массива указателей
     int *rows;      // указатель для блока памяти для хранения информации по строкам
  
-    int rowscount;  // количество строк
-    int d;      // вводимое число
-    int i, j=0;
+    int n, m = 0; // Последнее число
+    int i, j = 0; // Текущее число
     // ввод количества строк
     printf("Rows count=");
-    scanf("%d", &rowscount);
- 
+    scanf("%d", &n);
     // выделяем память для двухмерного массива
-    table = calloc(rowscount, sizeof(int*));
-    rows = malloc(sizeof(int)*rowscount);
+    table = calloc(n, sizeof(int*));
+    rows = malloc(sizeof(int)*n);
 
-    matrix(table, rows, d, rowscount, i);    
+    matrix(table, rows, n, m, i, j);    
     // освобождение памяти
     free(table);
     free(rows);
@@ -27,19 +25,19 @@ int main(void)
     return 0;
 }
 
-int matrix(int **table, int *rows, int d, int rowscount, int i){
-    if(i<rowscount&&!table[i]){
+int matrix(int **table, int *rows, int n, int m, int i, int j){
+    if(i<n&&!table[i]){
         printf("\nColumns count for row %d=", i);
         scanf("%d", &rows[i]);
         table[i] = calloc(rows[i], sizeof(int));
         int j=0;
-        matrix_table(table, rows, d, i, j);
+        matrix_table(table, rows, n, m, i, j);
         i++;
-        matrix(table, rows, d, i, j);
+        matrix(table, rows, n, m, i, j);
     }
-    else if(i<rowscount&&table[i]) {
+    else if(i<n&&table[i]) {
         printf('\n');
-        matrix_table(table, rows, d, i, j);
+        matrix_table(table, rows, n, m, i, j);
     }
     else{
         i=0;
@@ -48,11 +46,11 @@ int matrix(int **table, int *rows, int d, int rowscount, int i){
     return 0;
 }
 
-int matrix_table(int **table, int *rows, int d, int i, int j) {
+int matrix_table(int **table, int *rows, int n, int m, int i, int j) {
     if(j<rows[i]&&!table[i][j]){
         printf("table[%d][%d]=", i, j);
-        scanf("%d", &d);
-        table[i][j] = d;
+        scanf("%d", &m);
+        table[i][j] = m;
     }
     else if(j<rows[i]&&table[i][j]) {
         printf("%d \t", table[i][j]);
